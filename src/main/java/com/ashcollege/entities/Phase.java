9e825ef76a34ac88fd0780ef3fe6f5f89a5b4216
adarm12 <adarm1234@gmail.com>
2024-05-05 15:39:00 +0300

    1

--- src/main/java/com/ashcollege/entities/Phase.java
+++ src/main/java/com/ashcollege/entities/Phase.java
@@ -12,4 +12,67 @@ public class Phase {
     private PlayerPhase player4;
     private PlayerPhase player5;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public Play getPlay() {
+        return play;
+    }
+
+    public void setPlay(Play play) {
+        this.play = play;
+    }
+
+    public PlayerPhase getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(PlayerPhase player1) {
+        this.player1 = player1;
+    }
+
+    public PlayerPhase getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(PlayerPhase player2) {
+        this.player2 = player2;
+    }
+
+    public PlayerPhase getPlayer3() {
+        return player3;
+    }
+
+    public void setPlayer3(PlayerPhase player3) {
+        this.player3 = player3;
+    }
+
+    public PlayerPhase getPlayer4() {
+        return player4;
+    }
+
+    public void setPlayer4(PlayerPhase player4) {
+        this.player4 = player4;
+    }
+
+    public PlayerPhase getPlayer5() {
+        return player5;
+    }
+
+    public void setPlayer5(PlayerPhase player5) {
+        this.player5 = player5;
+    }
 }
